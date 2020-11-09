27bd68025a34cd1916de9f95997e1e24feeeac3a
eberj <eberj@DESKTOP-4SH6NRH>
2020-11-09 11:13:41 -0600

    made random generator, looped for comparisons, determined winner

--- RockPaperScissors/public_html/RockPaperScissors.js
+++ RockPaperScissors/public_html/RockPaperScissors.js
@@ -1,35 +1,24 @@
-function generateComputerChoice() {
-//generate a random number between 1 and 3
-//1 = rock, 2 = paper, 3 = scissors
-//the function should return the name of the computer's choice
+let weapons=['rock','paper','scissors'];
+let userChoice= prompt("Enter your weapon");
 
+if (weapons.includes(userChoice)) {
+    let computerChoice= weapons[Math.floor(Math.random() * 3 + 1)];
+    let you= userChoice;
 }
-
-function PlayTheGame() {
-//declare variables to hold the player choice and the computer choice
-
-
-
-//use getElementsByName to get the form radio button values
-//loop through the array to find which one is checked
-//store the checked radio button's value in the variable for player choice
-
-
-    
-//Call the generateComputerChoice function and store the returned value
-//in the variable for computer choice
-
-
-
-//compare the player's choice and the computer's choice to determine the winner
-//rock beats scissors, scissors beat paper, paper beats rock
+if (computerChoice===you) {
+    alert(`Tie, they chose ${(computerChoice)} too`);
+} else {
+    if (
+        (you === 'scissors' && computerChoice === 'paper') ||
+        (you === 'scissors' && computerChoice === 'rock') ||
+        (you === 'rock' && computerChoice === 'scissors')
+      ) {      
+        alert(`YOU WIN!, they chose ${(computerChoice)}!`);
+      } else {
+        alert(`YOU LOSE :(, they chose ${(computerChoice)}!`);
+      }
+    }
+  
 
 
-
-//use an alert box to tell the player who won, or if it was a tie
-//when displaying the winner, display both the computer choice and the
-//player choice in your message
-
-
-}
 
